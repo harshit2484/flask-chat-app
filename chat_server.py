cdb30033185f974109f3a14ffd1820f48d9788e6
HARSHIT RATHOD <102988390+harshit2484@users.noreply.github.com>
2025-07-18 16:57:57 +0530

    Update chat_server.py

--- chat_server.py
+++ chat_server.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
+import eventlet
+
+eventlet.monkey_patch()  # Important for eventlet to work properly
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
 VALID_PASSWORD = "HARI"  # Change your secure password here
 
 @app.route('/')
 def index():
-    return render_template('chat.html')
+    return render_template('chat.html')  # Make sure chat.html is in 'templates' folder
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -27,4 +30,4 @@ def handle_message(data):
     emit('message', data, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
